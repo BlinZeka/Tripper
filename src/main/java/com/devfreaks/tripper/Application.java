be507f8b4936efc1d3316431c5d246013bc20ba2
Flakron Bytyqi <flakron@gmail.com>
2016-03-18 11:33:37 +0100

    sample javafx ui window

---
+++ src/main/java/com/devfreaks/tripper/Application.java
@@ -0,0 +1,38 @@
+package com.devfreaks.tripper;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    public static void main(String... args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Button button = new Button("Hello");
+        button.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent t) {
+                System.out.println("aber hallo");
+            }
+            
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(button);
+        
+        Scene scene = new Scene(root, 400, 300);
+        
+        stage.setTitle("Hello world");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+}
